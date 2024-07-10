820f2881723ff4d259c04aa80440ae8cfb07db8b
danicretense <168598759+danicretense@users.noreply.github.com>
2024-07-10 13:50:16 -0300

    Update BatePonto.py

--- Manipulando arquivos/BatePonto.py
+++ Manipulando arquivos/BatePonto.py
@@ -1,22 +1,12 @@
-import csv
-from datetime import datetime
-
-def pega_data(data, df):
-    """Escreve a data e o horário de entrada e saída no arquivo CSV."""
-    with open('Folha de ponto.csv', 'a', newline='') as novo:
-        escrevendo = csv.writer(novo)
-        escrevendo.writerow([data, df])
-
-def ponto_entrada(entrada):
-    """Guarda a data e horário de entrada no arquivo de texto."""
-    with open('entradas.txt', 'w') as guardando:
-        guardando.write(entrada)
-
 def lendo_arquivo():
-    """Lê a data e horário de entrada do arquivo de texto."""
+    """pega a data e a hora que esta guardada na função ponto_entrada"""
     with open('entradas.txt', 'r') as lendo:
         valores = lendo.readline().strip()
     return valores
+def covertendo(entrada):
+    c1=datetime.strptime(entrada,'%d/%m/%Y %H:%M')
+   
+    return c1
 
 data_formatada = None
 condicao = input('Ponto de entrada ou de saída? (e/s) ').strip().lower()
@@ -24,12 +14,13 @@ condicao = input('Ponto de entrada ou de saída? (e/s) ').strip().lower()
 if condicao == 'e':
     nome = input('Digite seu nome: ')
     data = datetime.now()
-    data_formatada = data.strftime('%d/%m %H:%M')
+    data_formatada = data.strftime('%d/%m/%Y %H:%M')
     ponto_entrada(data_formatada)
 elif condicao == 's':
     nome = input('Digite seu nome novamente: ')
     da = datetime.now()
-    df = da.strftime('%d/%m %H:%M')
+    df = da.strftime('%d/%m/%Y %H:%M')
     pega_data(lendo_arquivo(), df)
+    print(covertendo(lendo_arquivo()),da)
 else:
     print('Opção inválida. Digite "e" para entrada ou "s" para saída.')
