1006a4e5067ac4849b8366cefbf84b94d37b8389
danicretense <168598759+danicretense@users.noreply.github.com>
2024-07-11 18:52:31 -0300

    Update BatePonto.py

--- Manipulando arquivos/BatePonto.py
+++ Manipulando arquivos/BatePonto.py
@@ -1,12 +1,26 @@
+import csv
+from datetime import datetime
+
+def pega_data(data, df):
+    """Escreve a data e o horário de entrada e saída no arquivo CSV."""
+    with open('Folha de ponto.csv', 'w', newline='') as novo:
+        escrevendo = csv.writer(novo)
+        escrevendo.writerow([data, df])
+
+def ponto_entrada(entrada):
+    """Guarda a data e horário de entrada no arquivo de texto."""
+    with open('entradas.txt', 'w') as guardando:
+        guardando.write(entrada)
+
 def lendo_arquivo():
     """pega a data e a hora que esta guardada na função ponto_entrada"""
     with open('entradas.txt', 'r') as lendo:
         valores = lendo.readline().strip()
     return valores
-def covertendo(entrada):
+def covertendo(saida,entrada):
     c1=datetime.strptime(entrada,'%d/%m/%Y %H:%M')
-   
-    return c1
+    diferenca= saida-c1
+    return diferenca
 
 data_formatada = None
 condicao = input('Ponto de entrada ou de saída? (e/s) ').strip().lower()
@@ -21,6 +35,7 @@ elif condicao == 's':
     da = datetime.now()
     df = da.strftime('%d/%m/%Y %H:%M')
     pega_data(lendo_arquivo(), df)
-    print(covertendo(lendo_arquivo()),da)
+    print(covertendo(da,lendo_arquivo()))
 else:
     print('Opção inválida. Digite "e" para entrada ou "s" para saída.')
+ 
