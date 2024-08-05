91eb1a2f18231a1ef8e9ad48553766a1b05580fd
Cogumelo <danicretense@gmail.com>
2024-08-04 23:21:02 -0300

    Conectado ao banco de dados

--- Manipulando arquivos/BatePonto.py
+++ Manipulando arquivos/BatePonto.py
@@ -1,11 +1,12 @@
 import os
 import csv
-from datetime import datetime, timedelta
+from datetime import datetime
 import tkinter as tk
 from tkinter import ttk
 import customtkinter
 from PIL import Image, ImageTk
 from tkinter import messagebox
+import mysql.connector
 
 def main():
     def cabecalho():
@@ -13,7 +14,7 @@ def main():
             cabecalho_ = csv.writer(file)
             cabecalho_.writerow(["INICIO", "TERMINO", "TEMPO ESTUDADO"])
 
-    # cabecalho()
+    cabecalho()
     def covertendo(saida, entrada):
         c1 = datetime.strptime(entrada, '%d/%m/%Y %H:%M:%S')
         diferenca = saida - c1
@@ -41,11 +42,103 @@ def main():
         diferenca = covertendo(datetime.strptime(df, '%d/%m/%Y %H:%M:%S'), data)
         with open('Folha de ponto.csv', 'a', newline='') as novo:
             csv.register_dialect('my_dialect', delimiter=';')
-            escrevendo = csv.writer(novo,dialect='my_dialect')
-            escrevendo.writerow([data, df,formatar_timedelta(diferenca)])
+            escrevendo = csv.writer(novo, dialect='my_dialect')
+            escrevendo.writerow([data, df, formatar_timedelta(diferenca)])
 
     def create_interface():
-        # Criação da janela principal dd
+        # Função para registrar o usuário
+        def registrando():
+            usuario = entrada1.get()
+            cd_user = entrada2.get()
+            if not usuario or not cd_user:
+                messagebox.showwarning("FLOOR DIZ:", "Preencha todos os campos.")
+                return
+
+            conexao = mysql.connector.connect(
+                host='localhost',
+                user='root',
+                password='765FHJyjd$_@',
+                database='db_floor',
+            )
+            cursor = conexao.cursor()
+
+            comando = 'INSERT INTO usuarios (nome_user, cd) VALUES (%s, %s)'
+            valores = (usuario, cd_user)
+            
+            try:
+                cursor.execute(comando, valores)
+                conexao.commit()
+                messagebox.showinfo("FLOOR DIZ:", "Usuário criado com sucesso!")
+            except mysql.connector.Error as err:
+                messagebox.showerror("FLOOR DIZ:", f"Erro: {err}")
+            finally:
+                cursor.close()
+                conexao.close()
+
+        # Função para o comando de registro de entrada/saída
+        def comando():
+            valor = entry2.get()
+            conexao = mysql.connector.connect(
+                host='localhost',
+                user='root',
+                password='765FHJyjd$_@',
+                database='db_floor',
+            )
+            cursor = conexao.cursor()
+            consulta = 'SELECT * FROM usuarios'
+            cursor.execute(consulta)
+            registros = cursor.fetchall()
+            cursor.close()
+            conexao.close()
+
+            for i in registros:
+                registro = i[1]
+                if registro == valor:
+                    escolhido = combobox.get()
+                    if escolhido == "INICIO":
+                        data = datetime.now()
+                        data_formatada = data.strftime('%d/%m/%Y %H:%M:%S')
+                        ponto_entrada(data_formatada)
+                        messagebox.showinfo("FLOOR DIZ: ", "REGISTRADO!")
+                    else:
+                        da = datetime.now()
+                        df = da.strftime('%d/%m/%Y %H:%M:%S')
+                        entrada = lendo_arquivo()
+                        pega_data(entrada, df)
+                        diferenca = covertendo(da, entrada)
+                        formatar_timedelta(diferenca)
+                        messagebox.showinfo("FLOOR DIZ:", "REGISTRADO!")
+                        os.remove("entradas.txt")
+                    return
+            messagebox.showinfo("FLOOR DIZ:", "USUARIO NÃO ENCONTRADO")
+
+        def voltar():
+            combobox.place(x=440, y=300, width=500, height=35)
+            entry2.place(x=440, y=350, width=500, height=35)
+            butao.place(x=630, y=420)
+            cria_user.place(x=630, y=500)
+            butao_sair.place(x=650, y=460)
+            entrada1.place_forget()
+            entrada2.place_forget()
+            bt_voltar.place_forget()
+            criando.place_forget()
+
+        def janela_registro():
+            combobox.place_forget()
+            entry2.place_forget()
+            butao.place_forget()
+            cria_user.place_forget()
+            entrada1.place(x=440, y=300)
+            entrada1._set_appearance_mode('light')
+            entrada2.place(x=440, y=350)
+            entrada2._set_appearance_mode('light')
+            criando.place(x=630, y=420)
+            bt_voltar.place(x=630, y=500)
+        
+        def fechar_janela_principal():
+            root.destroy()
+
+        # Criação da janela principal
         root = tk.Tk()
         root.geometry("1994x834")  # Define o tamanho da janela (ajuste conforme necessário)
         root.title("FLOOR")
@@ -62,74 +155,25 @@ def main():
         combobox.set("INICIO")
         
         # Adicionar o campo de entrada de texto
-        entry2 =ttk.Entry(root)
+        entry2 = ttk.Entry(root)
         entry2.place(x=440, y=350, width=500, height=35)
-        
-        
-        def comando():
-            very = entry2.get().title()
-            if very == "Daniela":
-                escolhido = combobox.get()
-                if escolhido == "INICIO":
-                    data = datetime.now()
-                    data_formatada = data.strftime('%d/%m/%Y %H:%M:%S')
-                    ponto_entrada(data_formatada)
-                    messagebox.showinfo("FLOOR DIZ: ", "REGISTRADO!")
-                else:
-                    da = datetime.now()
-                    df = da.strftime('%d/%m/%Y %H:%M:%S')
-                    entrada = lendo_arquivo()
-                    pega_data(entrada, df)
-                    diferenca = covertendo(da, entrada)
-                    formatar_timedelta(diferenca)
-                    messagebox.showinfo("FLOOR DIZ:", "REGISTRADO!")
-                    os.remove("entradas.txt")
-            else:
-                messagebox.showinfo("FLOOR DIZ:", "NOME DE USUÁRIO INCORRETO")
-
-        def voltar():
-            combobox.place(x=440, y=300, width=500, height=35)
-            entry2.place(x=440, y=350, width=500, height=35)
-            butao.place(x=630, y=420)
-            cria_user.place(x=630, y=500)
-            butao_sair.place(x=650, y=460)
-            entrada1.place_forget()
-            entrada2.place_forget()
-            bt_voltar.place_forget()
-            criando.place_forget()
-
-        entrada1 = customtkinter.CTkEntry(root, placeholder_text='DIGITE SEU NOME: ', width=500, height=35)
-        entrada2 = customtkinter.CTkEntry(root, placeholder_text='CRIE SEU RE: ', width=500, height=35)
-        bt_voltar = customtkinter.CTkButton(root, text="VOLTAR", command=voltar)
-        criando = customtkinter.CTkButton(root, text='SALVAR')
-        
-        def janela_registro():
-            combobox.place_forget()
-            entry2.place_forget()
-            butao.place_forget()
-            cria_user.place_forget()
-            entrada1.place(x=440, y=300)
-            entrada1._set_appearance_mode('light')
-            entrada2.place(x=440, y=350)
-            entrada2._set_appearance_mode('light')
-            criando.place(x=630, y=420)
-            bt_voltar.place(x=630, y=500)
         
         # Adicionar o botão
         butao = customtkinter.CTkButton(root, text="REGISTRAR", command=comando)
         butao.place(x=630, y=420)
-        
-        def fechar_janela_principal():
-            root.destroy()
-        
+
         # Adicionando outros botões
         butao_sair = customtkinter.CTkButton(root, text='SAIR', command=fechar_janela_principal, width=100)
         butao_sair.place(x=650, y=460)
-        
+
         cria_user = customtkinter.CTkButton(root, text="CRIAR USUÁRIO", command=janela_registro)
         cria_user.place(x=630, y=500)
-        
-        # Loop principal da interface gráfica
+
+        entrada1 = customtkinter.CTkEntry(root, placeholder_text='DIGITE SEU NOME: ', width=500, height=35)
+        entrada2 = customtkinter.CTkEntry(root, placeholder_text='CRIE SEU CÓDIGO DE USUARIO: ', width=500, height=35)
+        bt_voltar = customtkinter.CTkButton(root, text="VOLTAR", command=voltar)
+        criando = customtkinter.CTkButton(root, text='SALVAR', command=registrando)
+
         root.mainloop()
     
     create_interface()
